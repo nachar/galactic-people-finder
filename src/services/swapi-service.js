d54b5736057eef6b9a2e5c5c7932cb4fff05722a
hassennachar <hnachar@gmail.com>
2026-02-06 22:53:25 +0100

    feat: encode search

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,7 +1,7 @@
 import { BASE_URL } from '../global/constants.js';
 
 export const fetchStarWarsCharacterList = async (search = '') => {
-  const res = await fetch(`${BASE_URL}/people/?search=${search}`);
+  const res = await fetch(`${BASE_URL}/people/?search=${encodeURIComponent(search)}`);
 
   if (!res.ok) throw new Error(`Error ${res.status}: ${res.statusText}`);
 
